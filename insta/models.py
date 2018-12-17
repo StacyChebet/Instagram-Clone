0c7974f84a377744d7224ee22f011da85cff511d
StacyChebet <staceychebet23@gmail.com>
2018-12-17 23:24:00 +0300

    Add the poster foreign key of the user posting the image

--- insta/models.py
+++ insta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,7 +8,8 @@ class Profile(models.Model):
 
 
 class Image(models.Model):
-    image = models.ImageField()
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'photos/')
     name = models.CharField(max_length=40)
     caption = models.TextField()
     likes = models.IntegerField()
